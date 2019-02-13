d7ec1466f2cf764f2b0a50d7b03945394cff5ae0
r.duarte <ruipmduarte@hotmail.com>
2019-02-13 14:13:53 +0000

    Update Protect-ADobject.ps1

--- Protect-ADobject.ps1
+++ Protect-ADobject.ps1
@@ -1,7 +1,7 @@
 function Protect-Adobject {
     [CmdletBinding()]
     param (
-        # Name of the AD objects to un-protect Ex: objectname*
+        # Name of the AD objects to protect Ex: objectname*
         [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Mandatory = $true, HelpMessage = "Please specify object names to protect, can use wildcards")]
         [string]
         $adobjects
